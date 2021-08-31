16ca65e999d96e3a6fb33bc18baccb22a8d749eb
NoSoundLeR <slowercod7bo@gmail.com>
2021-08-31 21:48:12 +0300

    fix oauth username resolver

--- fastapi_auth/services/oauth.py
+++ fastapi_auth/services/oauth.py
@@ -1,4 +1,7 @@
 from fastapi_auth.repo import AuthRepo
+from fastapi_auth.utils.string import create_random_string
+
+MAX_ATTEMPTS = 15
 
 
 async def resolve_username(repo: AuthRepo, email: str) -> str:
@@ -12,5 +15,13 @@ async def resolve_username(repo: AuthRepo, email: str) -> str:
         if existing_username is None:
             return resolved_username
 
-        if i > 10_000:
+        i += 1
+
+        if i > MAX_ATTEMPTS:
+            postfix = create_random_string(6)
+            resolved_username = f"{username}{postfix}"
+            existing_username = await repo.get_by_username(resolved_username)
+            if existing_username is None:
+                return resolved_username
+
             raise RuntimeError("something is wrong")
