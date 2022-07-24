f96d7dd3d330be51e5c0714df0079dab1d083988
pegov <dvpegov@gmail.com>
2022-07-24 12:05:39 +0300

    fix redis backend init

--- fastapi_auth/backend/cache/redis.py
+++ fastapi_auth/backend/cache/redis.py
@@ -6,27 +6,23 @@ from fastapi_auth.backend.abc.cache import AbstractCacheClient
 
 
 class RedisClient(AbstractCacheClient):
-    def __init__(self, redis: Optional[Redis]) -> None:
-        if redis is not None:
-            self.set_client(redis)
-
-    def set_client(self, redis: Redis) -> None:
-        self._redis = redis
+    def __init__(self, conn: Redis) -> None:
+        self._conn = conn
 
     async def get(self, key: str) -> Optional[str]:
-        return await self._redis.get(key)
+        return await self._conn.get(key)
 
     async def delete(self, key: str) -> None:
-        await self._redis.delete(key)
+        await self._conn.delete(key)
 
     async def set(self, key: str, value: Union[str, bytes, int], ex: int) -> None:
-        await self._redis.set(key, value, ex=ex)
+        await self._conn.set(key, value, ex=ex)
 
     async def setnx(self, key: str, value: Union[str, bytes, int], ex: int) -> bool:
-        return bool(await self._redis.set(key, value, ex=ex, nx=True))
+        return bool(await self._conn.set(key, value, ex=ex, nx=True))
 
     async def incr(self, key: str) -> int:
-        return await self._redis.incr(key)
+        return await self._conn.incr(key)
 
     async def expire(self, key: str, ex: int) -> None:
-        await self._redis.expire(key, ex)
+        await self._conn.expire(key, ex)
